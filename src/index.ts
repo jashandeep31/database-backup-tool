4f8ba156552796d0537ce9a69b03bd3ef440ad8b
jashandeep <jashandeep1659@gmail.com>
2024-03-30 20:30:42 +0530

    first commit

---
+++ src/index.ts
@@ -0,0 +1,82 @@
+import dotenv from "dotenv";
+dotenv.config();
+import express from "express";
+import { exec } from "child_process";
+const PORT = process.env.PORT || 8000;
+import fs from "fs";
+import path from "path";
+import cron from "node-cron";
+
+const databasesArray: {
+  NAME: string;
+  HOST: string;
+  PORT: number;
+  USER: string;
+  PASSWORD: string;
+}[] = JSON.parse(process.env.DATABASES_ARRAY || "[]");
+
+const app = express();
+function main() {
+  databasesArray.map((item) => {
+    const dir = path.join(__dirname, "dumps");
+    if (!fs.existsSync(dir)) {
+      fs.mkdirSync(dir);
+    }
+    const fileName = `${item.NAME}-${new Date()
+      .toISOString()
+      .replace(/:/g, "-")}.sql`;
+    const filePath = path.join(dir, fileName);
+    exec(
+      `PGPASSWORD=${item.PASSWORD} pg_dump -h ${item.HOST} -p ${item.PORT} -U ${item.USER} -d ${item.NAME} > ${filePath}`,
+      async (error, stdout, stderr) => {
+        if (!error) {
+          const formData = new FormData();
+          formData.append("chat_id", process.env.GROUP_ID ?? "");
+          const fileBuffer = fs.readFileSync(filePath);
+          const blob = new Blob([fileBuffer]);
+          formData.append("document", blob, fileName);
+          formData.append(
+            "caption",
+            `Backup of ${
+              item.NAME
+            } \n ${new Date().toLocaleDateString()} \n ✅ ✅`
+          );
+          try {
+            await fetch(
+              `https://api.telegram.org/${
+                process.env.BOT_TOKEN ?? ""
+              }/sendDocument`,
+              {
+                method: "POST",
+                body: formData,
+              }
+            );
+          } catch (error) {}
+          fs.unlinkSync(filePath);
+        } else {
+          try {
+            await fetch(
+              `https://api.telegram.org/${
+                process.env.BOT_TOKEN ?? ""
+              }/sendMessage?chat_id=${
+                process.env.GROUP_ID
+              }&text=Failed to backup ${item.NAME} ❌`
+            );
+          } catch (error) {}
+          fs.unlinkSync(filePath);
+        }
+      }
+    );
+  });
+}
+
+cron.schedule(" * */6 * * *", () => {
+  try {
+    main();
+    console.log("first");
+  } catch (error) {}
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is firing up at 🔥 ${PORT}`);
+});
